ebab9ac6731b126dcecacd0638b110610aea02a3
sdc <github@software.unf.dk>
2014-07-15 16:24:03 +0200

    Adds combodectection to Tile.java Adds getTileAt to Grid.java -- Returns a tile from a grid at the specified x and y coordinates.

--- src/dk/sdc/Candy2048Crush/Grid.java
+++ src/dk/sdc/Candy2048Crush/Grid.java
@@ -8,10 +8,33 @@ import java.util.ArrayList;
 public class Grid {
     ArrayList<Tile> tileList = new ArrayList<Tile>();
 
+    /**
+     * Checks if a tile is adjacent to another
+     * @param tile1 First tile
+     * @param tile2 Second tile
+     * @return Returns true if tiles are adjacent
+     */
     public boolean isAdjacentTile(Tile tile1, Tile tile2) {
         if((Math.abs(tile1.getX()-tile2.getX()) == 1 && tile1.getY() == tile2.getY()) || (Math.abs(tile1.getY()-tile2.getY()) == 1 && tile1.getX() == tile2.getX())) {
             return true;
         }
         return false;
+    }
+
+    /**
+     * Gets the tile from the grid at the x and y coordinates
+     * @param x
+     * @param y
+     * @return Returns the tile found. If no tile is found will return a tile with a value of -1
+     */
+    public Tile getTileAt(int x, int y) {
+        for(int i = 0; i < tileList.size(); i++) {
+            Tile temp = tileList.get(i);
+            if(temp.getX() == x && temp.getY() == y) {
+                return tileList.get(i);
+            }
+        }
+
+        return new Tile(null,-1,-1,-1);
     }
 }
--- src/dk/sdc/Candy2048Crush/Tile.java
+++ src/dk/sdc/Candy2048Crush/Tile.java
@@ -8,12 +8,64 @@ public class Tile {
     private int x;
     private int y;
     private int value;
+    private boolean selected;
 
     public Tile(Grid parent, int x, int y, int value) {
         this.parent = parent;
         this.x = x;
         this.y = y;
         this.value = value;
+    }
+
+    /**
+     * Will detect whether the tile and adjacent tiles are in the correct position for a combo
+     * @return Returns true if a combo is found, otherwise returns false
+     */
+    public boolean isComboAvailable() {
+        int tilesFound = 1;
+        if(parent.getTileAt(x,y-1).value == value) {
+            tilesFound++;
+            if(parent.getTileAt(x,y-2).value == value) {
+                tilesFound++;
+            }
+        }
+        if(parent.getTileAt(x,y+1).value == value) {
+            tilesFound++;
+            if(parent.getTileAt(x,y+2).value == value) {
+                tilesFound++;
+            }
+        }
+        if(tilesFound >= 3) {
+            return true;
+        }
+
+        tilesFound = 1;
+
+        if(parent.getTileAt(x-1,y).value == value) {
+            tilesFound++;
+            if(parent.getTileAt(x-2,y).value == value) {
+                tilesFound++;
+            }
+        }
+        if(parent.getTileAt(x+1,y).value == value) {
+            tilesFound++;
+            if(parent.getTileAt(x+2,y).value == value) {
+                tilesFound++;
+            }
+        }
+        if(tilesFound >= 3) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
     }
 
     public int getX() {
