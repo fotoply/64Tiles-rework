564908dabde501636a71472b6bfb31d535374e6e
sdc <github@software.unf.dk>
2014-07-15 19:34:33 +0200

    Added GameActivity and made it accesable from MainActivity.

--- src/dk/sdc/Candy2048Crush/MainActivity.java
+++ src/dk/sdc/Candy2048Crush/MainActivity.java
@@ -1,8 +1,11 @@
 package dk.sdc.Candy2048Crush;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
     /**
@@ -12,5 +15,14 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button buStartGame = (Button)findViewById(R.id.buGameStart);
+        buStartGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
