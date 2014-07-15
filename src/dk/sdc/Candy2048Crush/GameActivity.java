cf7f63d052d6d2bba6c1e18e84c2c348b9af7f8d
sdc <github@software.unf.dk>
2014-07-15 19:24:20 +0200

    Fixed getX() and getY() in Tile.java to getxPos(), to avoid conflicting with the superclass of Tile.java, again

---
+++ src/dk/sdc/Candy2048Crush/GameActivity.java
@@ -0,0 +1,15 @@
+package dk.sdc.Candy2048Crush;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sdc on 7/15/14.
+ */
+public class GameActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game);
+    }
+}
